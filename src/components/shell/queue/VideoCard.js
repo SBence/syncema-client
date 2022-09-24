fc18753806823a8d70897b131de284df8c3906ec
SBence <43538202+SBence@users.noreply.github.com>
2022-09-24 12:57:52 +0200

    Add delete button on hover to video cards

--- src/components/shell/queue/VideoCard.js
+++ src/components/shell/queue/VideoCard.js
@@ -1,36 +1,75 @@
-import { Badge, Grid, Group, Image, Paper, Text, Title } from "@mantine/core";
+import {
+  Badge,
+  Button,
+  Grid,
+  HoverCard,
+  Image,
+  Paper,
+  Text,
+  Title,
+} from "@mantine/core";
+import { PlaylistX } from "tabler-icons-react";
 
 export default function VideoCard({ video }) {
   return (
-    <Paper
-      shadow="xs"
-      p="md"
-      sx={(theme) => ({
-        backgroundColor: theme.colors.dark[6],
-        "&:hover": {
-          backgroundColor: theme.colors.dark[5],
-        },
-      })}
-    >
-      <Grid grow>
-        <Grid.Col span={3}>
-          <Image radius="md" src={video.thumbnailURL} alt="Video thumbnail" />
-        </Grid.Col>
-        <Grid.Col span={9}>
-          <Group position="apart">
-            <Title order={5} style={{ textOverflow: "ellipsis" }}>
-              {video.title}
-            </Title>
-            <Badge
-              color={video.duration.toLowerCase() === "live" ? "red" : "gray"}
-              variant="filled"
+    <HoverCard shadow="md">
+      <HoverCard.Target>
+        <Paper
+          shadow="xs"
+          p="md"
+          sx={(theme) => ({
+            backgroundColor: theme.colors.dark[6],
+            "&:hover": {
+              backgroundColor: theme.colors.dark[5],
+            },
+          })}
+        >
+          <Grid grow>
+            <Grid.Col span={3}>
+              <Image
+                radius="md"
+                src={video.thumbnailURL}
+                alt="Video thumbnail"
+              />
+            </Grid.Col>
+            <Grid.Col
+              span={9}
+              style={{
+                minWidth: 0,
+              }}
             >
-              {video.duration}
-            </Badge>
-          </Group>
-          <Text>{`Queued by: ${video.queuedBy}`}</Text>
-        </Grid.Col>
-      </Grid>
-    </Paper>
+              <Title
+                order={5}
+                style={{
+                  textOverflow: "ellipsis",
+                  overflow: "hidden",
+                  whiteSpace: "nowrap",
+                }}
+              >
+                {video.title}
+              </Title>
+              <Badge
+                color={video.duration.toLowerCase() === "live" ? "red" : "gray"}
+                variant="filled"
+              >
+                {video.duration}
+              </Badge>
+              <Text
+                style={{
+                  textOverflow: "ellipsis",
+                  overflow: "hidden",
+                  whiteSpace: "nowrap",
+                }}
+              >{`Queued by: ${video.queuedBy}`}</Text>
+            </Grid.Col>
+          </Grid>
+        </Paper>
+      </HoverCard.Target>
+      <HoverCard.Dropdown>
+        <Button color="red" leftIcon={<PlaylistX />}>
+          Remove
+        </Button>
+      </HoverCard.Dropdown>
+    </HoverCard>
   );
 }
