5cb3d5cd6c382c5f1918f3549023ee399fe811d1
SBence <43538202+SBence@users.noreply.github.com>
2022-12-10 16:29:28 +0100

    Prepare local queue storage

--- src/components/shell/QueueContent.js
+++ src/components/shell/QueueContent.js
@@ -1,4 +1,11 @@
-import { ActionIcon, Space, Stack, TextInput, Title } from "@mantine/core";
+import {
+  ActionIcon,
+  Select,
+  Space,
+  Stack,
+  TextInput,
+  Title,
+} from "@mantine/core";
 import { useState } from "react";
 import { PlaylistAdd } from "tabler-icons-react";
 import VideoCard from "./queue/VideoCard.js";
@@ -28,9 +35,25 @@ export default function QueueContent({
     videoIndex += 1;
   }
 
+  const [value, setValue] = useState(null);
+  const [data, setData] = useState(["React", "Angular"]);
+
   return (
     <>
       <Title order={3}>Queue</Title>
+      {/* <Space h="xs" />
+      <Select
+        value={value}
+        onChange={setValue}
+        data={data}
+        searchable
+        creatable
+        getCreateLabel={(query) => `+ Save as: ${query}`}
+        onCreate={(query) => {
+          setData((current) => [...current, query]);
+          return query;
+        }}
+      /> */}
       <Space h="xs" />
       <Stack
         style={{
